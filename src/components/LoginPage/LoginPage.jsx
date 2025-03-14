2b394cd45d62bc60f417bdfb514c845003fa38aa
Hemkesh Agrawal <hemkeshagrawal@gmail.com>
2025-03-13 23:48:46 -0500

    HemkeshLudoV1

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,12 +1,18 @@
 import AddServer from './AddServer/AddServer';
 import JoinServer from './JoinServer/JoinServer';
+import logoImage from '../../images/logo.png';
 import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
     return (
         <div className={styles.container}>
-            <JoinServer />
-            <AddServer />
+            <div className={styles.logoContainer}>
+                <img src={logoImage} alt="Hemkesh Ludo" className={styles.logo} />
+            </div>
+            <div className={styles.gameOptionsContainer}>
+                <AddServer />
+                <JoinServer />
+            </div>
         </div>
     );
 };
